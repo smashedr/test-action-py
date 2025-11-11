98f9a4b44b922d85dde7f793d4c62beefd70fcfb
Shane <6071159+smashedr@users.noreply.github.com>
2025-11-11 02:06:21 -0800

    Retard 1

--- src/main.py
+++ src/main.py
@@ -49,6 +49,50 @@ print(f"sha: {sha}")
 
 # Action
 
+
+class GitHubContext:
+    def __init__(self):
+        for ctx in [
+            "action",
+            "action_ref",
+            "action_repository",
+            "actions",
+            "actor",
+            "actor_id",
+            "api_url",
+            "base_ref",
+            "env",
+            "event_name",
+            "event_path",
+            "graphql_url",
+            "head_ref",
+            "job",
+            "output",
+            "path",
+            "ref",
+            "ref_name",
+            "ref_protected",
+            "ref_type",
+            "repository",
+            "repository_id",
+            "repository_owner",
+            "repository_owner_id",
+            "retention_days",
+            "run_attempt",
+            "run_id",
+            "run_number",
+            "server_url",
+            "sha",
+            "step_summary",
+            "triggering_actor",
+            "workflow",
+            "workflow_ref",
+            "workflow_sha",
+            "workspace",
+        ]:
+            setattr(self, ctx, os.environ.get(f"GITHUB_{ctx}".upper()))
+
+
 contexts = [
     "action",
     "action_ref",
@@ -88,12 +132,16 @@ contexts = [
     "workspace",
 ]
 
-context = {}
-for ctx in contexts:
-    context[ctx] = os.environ.get(f"GITHUB_{ctx}".upper())
+# context = {}
+# for ctx in contexts:
+#     context[ctx] = os.environ.get(f"GITHUB_{ctx}".upper())
+
+context = GitHubContext()
+print(f"context.sha: {context.sha}")
+print(f"context.repository: {context.repository}")
 
 print("::group::GitHub Context Data")
-print(json.dumps(context, indent=4))
+print(vars(context))
 print("::endgroup::")
 
 
