ab90370b1faf1319348b8a92ccb43c0376b762ad
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-11-11 17:28:38 -0800

    Actions Tools (#1)

--- src/main.py
+++ src/main.py
@@ -1,85 +1,84 @@
 import json
-import os
 
 # noinspection PyPackageRequirements
-from actions import core
+from actions import context, core
 from github import Auth, Github, GithubException
 
 
-version = core.get_version()
-print(f"🏳️ Starting Python Test Action - \033[32;1m{version}")
-
-
-# Debug
-
-print(f"GITHUB_ACTION: {os.environ.get('GITHUB_ACTION')}")
-print(f"GITHUB_ACTION_REPOSITORY: {os.environ.get('GITHUB_ACTION_REPOSITORY')}")
-print(f"GITHUB_REF: {os.environ.get('GITHUB_REF')}")
-print(f"GITHUB_REF_NAME: {os.environ.get('GITHUB_REF_NAME')}")
+version: str = core.get_version()
+core.info(f"🏳️ Starting Python Test Action - \033[32;1m{version}")
 
 
 # Inputs
 
-tag = core.get_input("tag")
-print(f"tag: \033[36;1m{tag}")
+tag: str = core.get_input("tag")
+core.info(f"tag: \033[36;1m{tag}")
 summary: bool = core.get_bool("summary")
-print(f"summary: \033[33;1m{summary}")
+core.info(f"summary: \033[33;1m{summary}")
 token: str = core.get_input("token")
-print(f"token: \033[36;1m{token}")
-data: dict = core.get_data("data")
-print(f"data: \033[35;1m{data}")
+core.info(f"token: \033[36;1m{token}")
+data: dict = core.get_dict("data")
+core.info(f"data: \033[35;1m{data}")
 
+
+# Debug
 
 event: dict = core.get_event()
-print("::group::GitHub Event Data")
-print(json.dumps(event, indent=4))
-print("::endgroup::")
+core.info("::group::GitHub Event Data")
+core.info(json.dumps(event, indent=4))
+core.info("::endgroup::")
+
+
+ctx = {k: v for k, v in vars(context).items() if not k.startswith("__")}
+del ctx["os"]
+core.info("::group::GitHub Context Data")
+core.info(json.dumps(ctx, indent=4))
+core.info("::endgroup::")
+
 
 repository: dict = event.get("repository", {})
 full_name: str = repository.get("full_name", "")
-print(f"full_name: {full_name}")
-# owner: str = full_name.split("/")[0]
-# print(f"owner: {owner}")
-# repo: str = full_name.split("/")[1]
-# print(f"repo: {repo}")
+core.info(f"repository.full_name: {full_name}")
 
-sha: str = os.environ.get("GITHUB_SHA", "")
-print(f"sha: {sha}")
+core.info(f"context.repository_owner: {context.repository_owner}")
+core.info(f"context.repository_name: {context.repository_name}")
 
 
 # Action
 
+core.info(f"context.repository: {context.repository}")
+core.info(f"context.sha: {context.sha}")
+
 g = Github(auth=Auth.Token(token))
-r = g.get_repo(f"{full_name}")
-print(f"repo.name: {r.name}")
-print(f"repo.full_name: {r.full_name}")
+r = g.get_repo(f"{context.repository}")
+core.info(f"repo.name: {r.name}")
+core.info(f"repo.full_name: {r.full_name}")
 
 try:
     ref = r.get_git_ref(f"tags/{tag}")
-    if ref.object.sha != sha:
-        print(f"Updating: {tag} -> {ref.object.sha}")
-        ref.edit(sha, True)
+    if ref.object.sha != context.sha:
+        core.info(f"Updating: {tag} -> {ref.object.sha}")
+        ref.edit(context.sha, True)
         result = "Updated"
     else:
-        print(f"Unchanged: {tag} -> {ref.object.sha}")
+        core.info(f"Unchanged: {tag} -> {ref.object.sha}")
         result = "Unchanged"
 
 except GithubException:
-    ref = r.create_git_ref(f"refs/tags/{tag}", sha)
-    print(f"Created: {ref.ref} -> {ref.object.sha}")
+    ref = r.create_git_ref(f"refs/tags/{tag}", context.sha)
+    core.info(f"Created: {ref.ref} -> {ref.object.sha}")
     result = "Created"
 
 g.close()
 
-print(f"ref.ref: {ref.ref}")
-print(f"ref.object.sha: {ref.object.sha}")
+core.info(f"ref.ref: {ref.ref}")
+core.info(f"ref.object.sha: {ref.object.sha}")
 
 
 # Outputs
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
 
-with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-    print(f"sha={sha}", file=f)  # type: ignore
+core.set_output("sha", ref.object.sha)
 
 
 # Summary
@@ -92,11 +91,10 @@ if summary:
         inputs_table.append(f"<tr><td>{x}</td><td>{value or '-'}</td></tr>")
     inputs_table.append("</table>")
 
-    with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
-        print("### Python Test Action", file=f)  # type: ignore
-        print(f"{result}: [{ref.ref}]({r.html_url}/releases/tag/{tag}) ➡️ `{sha}`", file=f)  # type: ignore
-        print(f"<details><summary>Inputs</summary>{''.join(inputs_table)}</details>\n", file=f)  # type: ignore
-        print(f"[Report an issue or request a feature]({r.html_url}/issues)", file=f)  # type: ignore
+    core.summary("### Python Test Action")
+    core.summary(f"{result}: [{ref.ref}]({r.html_url}/releases/tag/{tag}) ➡️ `{context.sha}`")
+    core.summary(f"<details><summary>Inputs</summary>{''.join(inputs_table)}</details>\n")
+    core.summary(f"[Report an issue or request a feature]({r.html_url}/issues)")
 
 
 print("✅ \u001b[32;1mFinished Success")
